b12d6a7bf6f0934794ddb8187a04de18996495f0
AfroComrade <72281933+AfroComrade@users.noreply.github.com>
2022-04-19 13:51:10 +1200

    HabitLogger Basics done
    Meets reqa I think
    I'd say 99% done to spec but isn't pretty

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,36 +2,37 @@
 using System.Data.SQLite;
 
 namespace HabitLogger
-{ 
+{
     class Program
     {
+        public static SQLHandler sqlHandler;
+
         public static void Main(String[] args)
         {
-            SQLHandler sqlHandler = new SQLHandler();
+            sqlHandler = new SQLHandler();
 
             Console.WriteLine("Hello, and Welcome to the Habit Logger!");
-            Console.WriteLine("How many Activities did you engage in today?");
+            Console.WriteLine("I hope you've been productive today!");
+            Console.WriteLine("Type 0 at any time to quit");
 
-            sqlHandler.printAll();
+            inputPerform(mainMenu());
 
-            int activities = intInput();
+            Console.WriteLine("Goodbye!");
+        }
 
-            for (int i = 0; i < activities; i++)
-            {
-                String activity = "";
-                int numHours = 0;
+        public static int mainMenu()
+        {
+            Console.WriteLine("\nWhat would you like to do?");
+            Console.WriteLine("1. Add Activity"); // Insert
+            Console.WriteLine("2. Delete a past activity"); // Delete
+            Console.WriteLine("3. Update a past activity"); // Update
+            Console.WriteLine("4. View all activities"); // View
 
-                Console.WriteLine("Activity " + i + ":");
-                activity = Console.ReadLine();
-                Console.WriteLine("Number of Hours Performed:");
-                numHours = intInput();
-
-                sqlHandler.insert(activity, numHours);
-            }
+            return intMenuOption();
         }
 
 
-        public static int intInput()
+        public static int intMenuOption()
         {
             String input = "";
             int number = 0;
@@ -39,21 +40,184 @@ namespace HabitLogger
             while (input.Equals(""))
             {
                 input = Console.ReadLine();
-                try
-                {
-                    int.TryParse(input, out number);
-                }
-                catch (FormatException e)
+
+                bool parsed = int.TryParse(input, out number);
+
+                if (!parsed)
                 {
                     Console.WriteLine("Need an integer!");
                     input = "";
                 }
+                else if (number < 0 || number > 4)
+                {
+                    Console.WriteLine("Need a number between 1 & 4!");
+                    input = "";
+                }
+
             }
             return number;
-
         }
 
-        
+        public static void inputPerform(int i)
+        {
+            int input = 0;
+            switch (i)
+            {
+                case 0:
+                    return;
+                case 1:
+                    input = addActivity();
+                    break;
+                case 2:
+                    input = deleteActivity();
+                    break;
+                case 3:
+                    input = updateActivity();
+                    break;
+                case 4:
+                    sqlHandler.printAll();
+                    input = 1;
+                    break;
+            }
+
+            if (input == 0)
+                return;
+            else
+                inputPerform(mainMenu());
+        }
+
+
+        public static int updateActivity()
+        {
+            Console.WriteLine("Which Log do you wish to delete?");
+            Console.WriteLine("(You can find your log ID by viewing all logs)");
+
+            String input = "";
+            int id = 0;
+
+            while (input.Equals(""))
+            {
+                input = Console.ReadLine();
+
+                bool parsed2 = int.TryParse(input, out id);
+                if (!parsed2 || id < 0)
+                {
+                    Console.WriteLine("Please enter a number below 0!");
+                    input = "";
+                }
+            }
+
+            if (id == 0) // Always quit if the user enters 0 in the prompt
+                return 0;
+
+            // Logic for handling if the id exists
+            Boolean found = false;
+            for (int i = 0; i < sqlHandler.numElements; i++) 
+            {
+                if (id == sqlHandler.ids[i])
+                    found = true;
+            }
+            if (!found)
+            {
+                Console.WriteLine("ID not found. Please view all logs and try again.");
+                return id;
+            }
+
+
+            String newActivity = "";
+
+            int check = -1;
+            Console.WriteLine("New activity?");
+            newActivity = Console.ReadLine();
+            bool parsed = int.TryParse(newActivity, out check);
+            
+            if (parsed && check == 0)
+                    return 0;
+
+            int newTimesPerformed = 0;
+            input = "";
+            Console.WriteLine("Times performed?");
+            while (input.Equals(""))
+            {
+                input = Console.ReadLine();
+                parsed = int.TryParse(input, out newTimesPerformed);
+
+                if (parsed && newTimesPerformed == 0)
+                    return newTimesPerformed;
+                else if (!parsed)
+                {
+                    Console.WriteLine("Please enter a number!");
+                }
+            }
+
+            sqlHandler.update(id, newActivity, newTimesPerformed);
+
+            return 1;
+        }
+
+        public static int addActivity()
+        {
+            int timesPerformed = -1;
+            Console.WriteLine("Activity :");
+            String activity = Console.ReadLine();
+
+            bool parsed = int.TryParse(activity, out timesPerformed);
+
+            if (parsed && timesPerformed == 0)
+                return 0;
+
+
+            Console.WriteLine("Number of times Performed:");
+            String input = "";
+            while (input.Equals(""))
+            {
+                input = Console.ReadLine();
+                parsed = int.TryParse(input, out timesPerformed);
+
+                if (!parsed || timesPerformed < 0)
+                {
+                    Console.WriteLine("Need an integer above 0!");
+                    input = "";
+                }
+            }
+                
+            if (timesPerformed != 0)
+                sqlHandler.insert(activity, timesPerformed);
+            
+            return timesPerformed;
+        }
+
+        public static int deleteActivity()
+        {
+            Console.WriteLine("Which Log do you wish to delete?");
+            Console.WriteLine("(You can find your log ID by viewing all logs)");
+
+            String input = "";
+            int id = 0;
+            while (input.Equals(""))
+            {
+                input = Console.ReadLine();
+                
+                bool parsed = int.TryParse(input, out id);
+
+                if (!parsed || id < 0)
+                {
+                    Console.WriteLine("Need a number above 0!");
+                    input = "";
+                }
+            }
+
+            Boolean deleted = false;
+            if (id != 0)
+                deleted = sqlHandler.deleteID(id);
+
+            if (deleted)
+                Console.WriteLine(id + " successfully deleted;");
+            else
+                Console.WriteLine(id + " not found. Please view all and try again, or report deletion fail to admin");
+
+            return id;
+        }
     }
 }
 
--- ConsoleApp1/SQLHandler.cs
+++ ConsoleApp1/SQLHandler.cs
@@ -6,6 +6,8 @@ namespace HabitLogger
     internal class SQLHandler
     {
         private SQLiteCommand command;
+        public int[] ids = new int[99];
+        public int numElements = 0;
 
         public SQLHandler()
         {
@@ -16,28 +18,82 @@ namespace HabitLogger
 
             command = conn.CreateCommand();
             command.CommandText =
-                "CREATE TABLE IF NOT EXISTS Logs([Id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, [Activity] NVARCHAR(64) NOT NULL, [Duration] INTEGER NOT NULL)";
+                "CREATE TABLE IF NOT EXISTS Logs([Id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, [Activity] NVARCHAR(64) NOT NULL, [TimesPerformed] INTEGER NOT NULL)";
             command.ExecuteNonQuery();
+
+            initializeIDArray();
         }
 
-        public void insert(String activity, int Duration)
+        public void initializeIDArray()
         {
-            command.CommandText = "INSERT INTO Logs(Activity, Duration) VALUES('" + activity + "', '" + Duration + "') ";
+            ids = new int[20];
+
+            command.CommandText = "SELECT ID FROM Logs";
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    ids[numElements] = reader.GetInt32(0);
+                    numElements++;
+                }
+            }
+        }
+
+        public void insert(String activity, int timesPerformed)
+        {
+            command.CommandText = "INSERT INTO Logs(Activity, TimesPerformed) VALUES('" + activity + "', '" + timesPerformed + "') ";
             command.ExecuteNonQuery();
+            initializeIDArray();
+        }
+
+        public Boolean update(int id, String activity, int timesPerformed)
+        {
+            command.CommandText = $"UPDATE Logs SET Activity = '{activity}', TimesPerformed = '{timesPerformed}' WHERE id = {id}";
+            command.ExecuteNonQuery();
+            return true;
+        }
+
+        public Boolean deleteID(int id)
+        {
+            Boolean found = false;
+            for (int i = 0; i < numElements; i++)
+            {
+                if (ids[i] == id)
+                    found = true;
+            }
+
+            if (found)
+            {
+                command.CommandText = "DELETE FROM Logs WHERE Id = " + id;
+                command.ExecuteNonQuery();
+                initializeIDArray();
+            }
+            return found;
+        }
+
+        public void printOne(int id)
+        {
+            command.CommandText = "SELECT ID, Activity, TimesPerformed FROM Logs WHERE ID = " + id;
+            using (var reader = command.ExecuteReader())
+            {
+                var activity = reader.GetString(1);
+                var timesPerformed = reader.GetInt32(2);
+                Console.WriteLine($": ID: {id}, {activity}, {timesPerformed} times performed"); ;
+            }
+            
         }
 
         public void printAll()
         {
-            command.CommandText = "SELECT Activity, Duration FROM Logs";
+            command.CommandText = "SELECT ID, Activity, TimesPerformed FROM Logs";
             using (var reader = command.ExecuteReader())
             {
-                int logNum = 0;
                 while (reader.Read())
                 {
-                    logNum++;
-                    var activity = reader.GetString(0);
-                    var duration = reader.GetInt32(1);
-                    Console.WriteLine("Log " + logNum + $": {activity}, {duration} hours");
+                    var id = reader.GetInt32(0);
+                    var activity = reader.GetString(1);
+                    var timesPerformed = reader.GetInt32(2);
+                    Console.WriteLine($"ID: {id}, {activity}, {timesPerformed} times performed");
                 }
             }
         }
